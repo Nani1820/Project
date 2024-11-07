31e83555bc870845c3534f07286121fff6785494
Nani1820 <nani4me110@gmail.com>
2024-11-07 17:55:14 -0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,4 +39,20 @@ dependencies {
     implementation(project(":data"))
     implementation(project(":domain"))
     implementation(project(":presentation"))
+
+    // For Robolectric (JVM-based unit tests)
+    testImplementation 'org.robolectric:robolectric:4.9'
+
+    // For Espresso (UI testing)
+    androidTestImplementation 'androidx.test.espresso:espresso-core:3.4.0'
+    androidTestImplementation 'androidx.test:runner:1.4.0'
+    androidTestImplementation 'androidx.test:rules:1.4.0'
+
+    // For Navigation testing
+    androidTestImplementation 'androidx.navigation:navigation-testing:2.5.0'
+    androidTestImplementation 'androidx.fragment:fragment-testing:1.5.0'
+
+    // For Kover (Test coverage)
+    testImplementation 'org.jetbrains.kotlinx:kover-api:0.7.0-Beta'
+    testImplementation 'org.jetbrains.kotlinx:kover-agent:0.7.0-Beta'
 }
